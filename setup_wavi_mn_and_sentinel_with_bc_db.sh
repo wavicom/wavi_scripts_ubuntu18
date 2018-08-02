e3317666050d1d7158d76f1df6f97022c3869a4e
wavicom <40639092+wavicom@users.noreply.github.com>
2018-08-03 01:17:59 +0500

    Update setup_wavi_mn_and_sentinel_with_bc_db.sh

--- setup_wavi_mn_and_sentinel_with_bc_db.sh
+++ setup_wavi_mn_and_sentinel_with_bc_db.sh
@@ -8,21 +8,21 @@ function randpw {
 
 function updates {
 	echo -e "$tyellow""Preparing the VPS to setup. It's gonna take some time."
-	apt-get update >/dev/null 2>&1
-	DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-	DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
-	apt install -y software-properties-common >/dev/null 2>&1
+	apt-get update
+	DEBIAN_FRONTEND=noninteractive apt-get update
+	DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade
+	apt install -y software-properties-common
 	echo -e "$tgreen""Complete"
 	echo -e "$tyellow""Adding bitcoin PPA repository"
-	apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
+	apt-add-repository -y ppa:bitcoin/bitcoin
 	echo -e "$tgreen""Complete"
 	echo -e "$tyellow""Installing required packages, it may take some time to finish"
-	apt-get update >/dev/null 2>&1
-	apt-get install libzmq3-dev -y >/dev/null 2>&1
-	apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
-	build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
-	libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-	libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ unzip libzmq5 >/dev/null 2>&1
+	apt-get update
+	apt-get install libzmq3-dev -y
+	apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common
+	build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev
+	libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev
+	libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ unzip libzmq5
 	echo -e "$tgreen""Complete"
 	if [ "$?" -gt "0" ];
 		then
@@ -31,9 +31,13 @@ function updates {
 		echo "apt -y install software-properties-common"
 		echo "apt-add-repository -y ppa:bitcoin/bitcoin"
 		echo "apt-get update"
-		echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
-		libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
+		echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev
+		libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev
 		bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libdb5.3++ unzip libzmq5"
 	exit 1
 	fi
 }
+
+clear
+echo "$tgreen""Masternode Install Script for WAVI"
+updates
